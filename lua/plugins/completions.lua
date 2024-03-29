e5a7ce28cefa16e71158a3218302ee00f3c14b28
lTriNiTyl <antoniokr@gmail.com>
2024-03-29 19:21:29 +0900

    "tailwindcss color표시되도록 수정하면서 formatting, icon 추가"

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -6,6 +6,10 @@ return {
     'hrsh7th/nvim-cmp',
     event = "InsertEnter",
     dependencies = {
+      "onsails/lspkind.nvim",
+      'hrsh7th/cmp-vsnip',
+      'hrsh7th/vim-vsnip',
+      { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
       "Exafunction/codeium.nvim",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
@@ -14,6 +18,51 @@ return {
       "rafamadriz/friendly-snippets",
     },
     config = function()
+      local kind_icons = {
+        Text = "",
+        Method = "󰆧",
+        Function = "󰊕",
+        Constructor = "",
+        Field = "󰇽",
+        Variable = "󰂡",
+        Class = "󰠱",
+        Interface = "",
+        Module = "",
+        Property = "󰜢",
+        Unit = "",
+        Value = "󰎠",
+        Enum = "",
+        Keyword = "󰌋",
+        Snippet = "",
+        Color = "󰏘",
+        File = "󰈙",
+        Reference = "",
+        Folder = "󰉋",
+        EnumMember = "",
+        Constant = "󰏿",
+        Struct = "",
+        Event = "",
+        Operator = "󰆕",
+        TypeParameter = "󰅲",
+        Codeium = "",
+      }
+      -- gray
+      vim.api.nvim_set_hl(0, 'CmpItemAbbrDeprecated', { bg = 'NONE', strikethrough = true, fg = '#808080' })
+      -- blue
+      vim.api.nvim_set_hl(0, 'CmpItemAbbrMatch', { bg = 'NONE', fg = '#569CD6' })
+      vim.api.nvim_set_hl(0, 'CmpItemAbbrMatchFuzzy', { link = 'CmpIntemAbbrMatch' })
+      -- light blue
+      vim.api.nvim_set_hl(0, 'CmpItemKindVariable', { bg = 'NONE', fg = '#9CDCFE' })
+      vim.api.nvim_set_hl(0, 'CmpItemKindInterface', { link = 'CmpItemKindVariable' })
+      vim.api.nvim_set_hl(0, 'CmpItemKindText', { link = 'CmpItemKindVariable' })
+      -- pink
+      vim.api.nvim_set_hl(0, 'CmpItemKindFunction', { bg = 'NONE', fg = '#C586C0' })
+      vim.api.nvim_set_hl(0, 'CmpItemKindMethod', { link = 'CmpItemKindFunction' })
+      -- front
+      vim.api.nvim_set_hl(0, 'CmpItemKindKeyword', { bg = 'NONE', fg = '#D4D4D4' })
+      vim.api.nvim_set_hl(0, 'CmpItemKindProperty', { link = 'CmpItemKindKeyword' })
+      vim.api.nvim_set_hl(0, 'CmpItemKindUnit', { link = 'CmpItemKindKeyword' })
+
       local cmp = require 'cmp'
       require('luasnip.loaders.from_vscode').lazy_load()
 
@@ -23,14 +72,34 @@ return {
         },
         snippet = {
           expand = function(args)
-            require('luasnip').lsp_expand(args.body)
+            vim.fn["vsnip#anonymous"](args.body)
             -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
             -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
             -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
           end,
         },
+        formatting = {
+          fields = { "kind", "abbr", "menu" },
+          format = function(entry, vim_item)
+            -- vim_item.kind = (kind_icons[vim_item.kind] or '?') .. ' ' .. vim_item.kind
+            local kind = vim_item.kind
+            vim_item.kind = (kind_icons[kind] or '?') .. " "
+            vim_item.menu = kind
+
+            --[[ local source = entry.source.name
+            vim_item.menu = " [" .. source .. "]" ]]
+
+            vim_item.formatter = require("tailwindcss-colorizer-cmp").formatter(entry, vim_item)
+            return vim_item
+          end,
+        },
         window = {
-          completion = cmp.config.window.bordered(),
+          completion = cmp.config.window.bordered({
+            -- winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+            border = "double",
+            side_padding = 0,
+            col_offset = -3
+          }),
           documentation = cmp.config.window.bordered(),
         },
         mapping = cmp.mapping.preset.insert({
@@ -45,12 +114,13 @@ return {
         sources = cmp.config.sources({
           { name = 'codeium' },
           { name = 'nvim_lsp' },
-          --{ name = 'vsnip' }, -- For vsnip users.
-          { name = 'luasnip' }, -- For luasnip users.
-          { name = 'buffer' },  -- text within current buffer
-          { name = 'path' },    -- file system paths
+          { name = 'vsnip' }, -- For vsnip users.
+          -- { name = 'luasnip' }, -- For luasnip users.
           -- { name = 'ultisnips' }, -- For ultisnips users.
           -- { name = 'snippy' }, -- For snippy users.
+        }, {
+          { name = 'buffer' }, -- text within current buffer
+          { name = 'path' },   -- file system paths
         }),
       })
     end
