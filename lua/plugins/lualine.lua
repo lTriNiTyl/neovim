7135c6189fa878d85c4317f69cff8d8af67d8ccd
lTriNiTyl <antoniokr@gmail.com>
2024-04-03 11:51:04 +0900

    "lualine을 이용해 statusline을 nvchad와 동일하게 변경."

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,163 @@
+-- show user name
+--[[ local name = function()
+  local os = require("os")
+  local username = os.getenv("USERNAME")
+  if not username then
+    username = os.getenv("USER")
+  end
+  return username
+end ]]
+
+local default_sep_icons = {
+  default = { left = "", right = " " },
+  round = { left = "", right = "" },
+  block = { left = "█", right = "█" },
+  arrow = { left = "", right = "" },
+}
+
+local folderName = function()
+  local sname = vim.fn.expand("%:.")
+  local kname = sname:match("([^/]+)/[^/]+$")
+  local c = {}
+  table.insert(c, kname)
+  return table.concat(c, '/')
+end
+
+-- 현재 파일 경로 알아오고, 상위폴더만 나타내는 함수
+--[[ local getScriptDirectory = function()
+  local scriptPath = debug.getinfo(1, "S").source:sub(2)
+  return scriptPath:match("(.*/)")
+end
+
+local cwd = function()
+  local currentDir = getScriptDirectory()
+  return currentDir:match(".*/([^/]+)/[^/]+/?$")
+  -- return tname:match(".*/([^/]+)/?$")
+end ]]
+
+-- LSP clients attached to buffer
+local clients_lsp = function()
+  local bufnr = vim.api.nvim_get_current_buf()
+
+  local clients = vim.lsp.buf_get_clients(bufnr)
+  if next(clients) == nil then
+    return ''
+  end
+
+  local c = {}
+  for _, client in pairs(clients) do
+    table.insert(c, client.name)
+  end
+  return '\u{f085}  LSP ~ ' .. table.concat(c, '|')
+end
+
 return {
   "nvim-lualine/lualine.nvim",
+  dependencies = {
+    "WhoIsSethDaniel/lualine-lsp-progress.nvim",
+  },
   config = function()
-   require('lualine').setup({
+    require('lualine').setup({
       options = {
+        -- section_separators = { left = '', right = '' },
+        component_separators = '',
         icons_enabled = true,
-        theme = 'OceanicNext',
+        theme = 'auto',
+        always_divide_middle = true,
+        globalstatus = false,
+      },
+      sections = {
+        lualine_a = {
+          {
+            'mode',
+            icons_enabled = true,
+            icon = '',
+            separator = default_sep_icons.default,
+          }
+        },
+        lualine_b = {
+          {
+            'filetype',
+            colored = true,
+            icon_only = true,
+            --[[ 'buffers',
+            show_filename_only = true,       -- Shows shortened relative path when set to false.
+            hide_filename_extension = false, -- Hide filename extension when set to true.
+            show_modified_status = true,     -- Shows indicator when the buffer is modified.
+
+            mode = 0,                        -- 0: Shows buffer name
+            max_length = vim.o.columns * 2 / 3,
+            filetype_names = {
+              TelescopePrompt = 'Telescope',
+              dashboard = 'Dashboard',
+              packer = 'Packer',
+              fzf = 'FZF',
+              alpha = 'Alpha'
+            }, ]]
+          },
+          {
+            'filename',
+            file_status = true,
+            newfile_status = false,
+            path = 0,
+            padding = 0,
+            separator = default_sep_icons.default,
+          }
+        },
+        lualine_c = {
+          { 'branch', icon = { '', align = 'left', --[[color = { fg = 'white' }]] } },
+          {
+            'diff',
+            colored = true,
+            diff_color = {
+              added = 'LuaLineDiffAdd',
+              modified = 'LuaLineDiffChange',
+              removed = 'LuaLineDiffDelete',
+            },
+            symbols = {
+              added = ' ',
+              modified = ' ',
+              removed = ' '
+            },
+          },
+        },
+        lualine_x = {
+          {
+            'diagnostics',
+            colored = true,
+            sources = {
+              'nvim_diagnostic',
+            },
+            symbols = {
+              error = ' ',
+              warn = ' ',
+              info = ' ',
+              hint = ' '
+            },
+          },
+          {
+            clients_lsp,
+            colored = true,
+          },
+        },
+        lualine_y = {
+          {
+            folderName,
+            icon = { "󰉋 ", align = 'center', color = { fg = 'black', bg = '#E06C75' } },
+            color = { fg = '', bg = '' }, -- #E06C75 #99C379
+            separator = { left = '' },
+            padding = { left = 0, right = 1 },
+          },
+        },
+        lualine_z = {
+          {
+            'progress',
+            icon = { "󰈚 ", align = 'center', color = { fg = 'black', bg = '#99C379' } },
+            color = { fg = '#99C379', bg = '' }, -- #E06C75 #99C379
+            separator = { left = '' },
+            padding = { left = 0, right = 1 },
+          }
+        },
       },
     })
   end
