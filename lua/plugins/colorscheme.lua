97716d5ff02817666de53b3fe173db0d1c328897
lTriNiTyl <antoniokr@gmail.com>
2024-04-10 15:11:40 +0900

    "Added tokyodark colorscheme"

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -9,25 +9,37 @@ return {
     end
   },
   {
+    "tiagovla/tokyodark.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      -- custom options here
+    },
+    -- config = function(_, opts)
+      -- require("tokyodark").setup(opts)   -- calling setup is optional
+      -- vim.cmd [[colorscheme tokyodark]]
+    -- end,
+  },
+  {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     opts = {},
---    config = function()
---      vim.cmd[[colorscheme tokyonight-storm]]
---    end
+    -- config = function()
+      -- vim.cmd [[colorscheme tokyonight-storm]]
+    -- end
   },
   {
     'navarasu/onedark.nvim',
     lazy = false,
     priority = 1000,
     opts = {},
---    config = function()
---      require('onedark').setup({
---        style = 'deep'
---      })
---      require('onedark').load()
---    end
+    --    config = function()
+    --      require('onedark').setup({
+    --        style = 'deep'
+    --      })
+    --      require('onedark').load()
+    --    end
   },
   {
     "rose-pine/neovim",
@@ -35,26 +47,26 @@ return {
     name = "rose-pine",
     priority = 1000,
     config = function()
---      require('rose-pine').setup({
---        variant = "moon"
---      })
---      vim.cmd [[colorscheme rose-pine]]
+      --      require('rose-pine').setup({
+      --        variant = "moon"
+      --      })
+      --      vim.cmd [[colorscheme rose-pine]]
     end
   },
   {
     'EdenEast/nightfox.nvim',
     lazy = false,
     priority = 1000,
---    config = function()
---      vim.cmd [[colorscheme Duskfox]]
---    end
+    --    config = function()
+    --      vim.cmd [[colorscheme Duskfox]]
+    --    end
   },
   {
     'rebelot/kanagawa.nvim',
     lazy = false,
     priority = 1000,
---    config = function()
---      vim.cmd [[colorscheme kanagawa]]
---    end
+    --    config = function()
+    --      vim.cmd [[colorscheme kanagawa]]
+    --    end
   },
 }
