45ad51a6f8491a9d0772717b8817f92669f93156
lTriNiTyl <antoniokr@gmail.com>
2024-04-13 18:46:21 +0900

    "neo-tree가 활성화 될때 열린 화일에 포커스되도록 수정"

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  event = 'Bufwipeout',
+  event = 'VimEnter',
   branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
@@ -10,8 +10,24 @@ return {
   config = function()
     -- vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
     require("neo-tree").setup({
+      enable_git_status = true,
+      enable_diagnostics = true,
       use_libuv_file_watcher = true,
       close_if_last_window = true,
+      sources = {
+        "filesystem",
+        "buffers",
+        "git_status",
+        "document_symbols"
+      },
+      buffers = {
+        follow_current_file = {
+          enable = true,
+        },
+      },
+      symbols = {
+        follow_cursor = true,
+      },
       window = {
         position = "left",
         width = 30,
